3dee6ea91c5b1566481d48aca01cb9b2d23a8f00
vysavya <vysu.vysavya@gmail.com>
2024-10-10 20:34:15 +0530

    updated rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,14 +1,15 @@
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai"; // Import star icons for rating display
 
 const Rating = ({ rating, onClick, style }) => {
   return (
     <>
+      {/* Create 5 stars for rating */}
       {[...Array(5)].map((_, i) => (
         <span key={i} onClick={() => onClick(i)} style={style}>
-          {rating > i ? (
+          {rating > i ? ( // Fill star if rating is greater than index
             <AiFillStar fontSize="15px" />
           ) : (
-            <AiOutlineStar fontSize="15px" />
+            <AiOutlineStar fontSize="15px" /> // Outline star if rating is less than or equal to index
           )}
         </span>
       ))}
